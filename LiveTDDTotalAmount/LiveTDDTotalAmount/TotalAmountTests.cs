db51618182361d4a3ac5637d2b8ea78ce374a5ad
coolwuu <coolwuu0207@gmail.com>
2018-03-03 16:39:14 +0800

    Init Project

---
+++ LiveTDDTotalAmount/LiveTDDTotalAmount/TotalAmountTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LiveTDDTotalAmount
+{
+    [TestClass]
+    public class TotalAmountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
